b3248a366b54886cb2aedc674e2f05771417b960
Your Name <you@example.com>
2022-05-26 20:57:43 +0800

    mutation

--- scripts/mutation/run_mutation.sh
+++ scripts/mutation/run_mutation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 img=/data/litszon/itf/ITF/2165-143-89-238-111.ap.ngrok.io/ImageSet
-label_path=/data/litszon/itf/ITF/2165-143-89-238-111.ap.ngrok.io/labels
-mutate_path=/data/litszon/itf/ITF/2165-143-89-238-111.ap.ngrok.io/mutate
+label=/data/litszon/itf/ITF/2165-143-89-238-111.ap.ngrok.io/labels
+mutate=/data/litszon/itf/ITF/2165-143-89-238-111.ap.ngrok.io/mutate
 mkdir -c /data/litszon/itf/ITF/2165-143-89-238-111.ap.ngrok.io/mutate
 python mutation_operation.py --image_path $img --label_path $label --mutate_path $mutate
