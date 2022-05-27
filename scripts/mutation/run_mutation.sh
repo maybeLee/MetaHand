b6928bc3f1b6a754600c35d218042fee4057c0d7
Your Name <you@example.com>
2022-05-27 12:53:38 +0800

    mutation

--- scripts/mutation/run_mutation.sh
+++ scripts/mutation/run_mutation.sh
@@ -5,6 +5,6 @@ img=/data/litszon/itf/ITF/2165-143-89-238-111.ap.ngrok.io/ImageSet/
 label=/data/litszon/itf/ITF/2165-143-89-238-111.ap.ngrok.io/labels/
 mutate=/data/litszon/itf/ITF/2165-143-89-238-111.ap.ngrok.io/mutate/
 mkdir -p /data/litszon/itf/ITF/2165-143-89-238-111.ap.ngrok.io/mutate
-python mutation_operation.py --image_path $img --label_path $label --mutate_path $mutate
+python -O mutation_operation.py --image_path $img --label_path $label --mutate_path $mutate
 
 ##on window
