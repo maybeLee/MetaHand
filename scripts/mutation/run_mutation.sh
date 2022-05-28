653f367bcf3ffedc5a9732a5e1363be50742342a
Your Name <you@example.com>
2022-05-28 15:23:11 +0800

    varying random erase

--- scripts/mutation/run_mutation.sh
+++ scripts/mutation/run_mutation.sh
@@ -6,6 +6,7 @@ label=/data/litszon/itf/ITF/2165-143-89-238-111.ap.ngrok.io/labels/
 mutate=/data/litszon/itf/ITF/2165-143-89-238-111.ap.ngrok.io/mutate/
 mkdir -p /data/litszon/itf/ITF/2165-143-89-238-111.ap.ngrok.io/mutate
 for rand_erase in 0.5 0.6 0.7 0.8 0.9 1.0
-python -O mutation_operation.py --image_path $img --label_path $label --mutate_path $mutate --random_erase $rand_erase
-
+do
+    python -O mutation_operation.py --image_path $img --label_path $label --mutate_path $mutate --random_erase $rand_erase
+done
 ##on window
