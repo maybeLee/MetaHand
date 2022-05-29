b0f7a8050945bd94b4b357308a4824c0f1109652
Your Name <you@example.com>
2022-05-29 22:02:27 +0800

    bug fix: make the script more efficient by reducing the number of parallel jobs

--- scripts/mutation/run_mutation.sh
+++ scripts/mutation/run_mutation.sh
@@ -7,9 +7,10 @@ mutate=/data/litszon/itf/ITF/2165-143-89-238-111.ap.ngrok.io/mutate/
 mkdir -p /data/litszon/itf/ITF/2165-143-89-238-111.ap.ngrok.io/mutate
 # rand_erase=0.0
 # guass_noise=0.0
-for rand_erase in 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0
+# for guass_noise in 0.0 0.1 0.5 1.0 2.0 4.0 8.0 16.0
+for guass_noise in 0.0 0.1 0.5
 do
-for guass_noise in 0.0 0.1 0.5 1.0 2.0 4.0 8.0 16.0
+for rand_erase in 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0
 do
     echo "fixMutRatio_fixXY\n"
     python -O mutation_operation.py --image_path $img --label_path $label --mutate_path $mutate --random_erase $rand_erase --random_erase_mode fixMutRatio_fixXY --guassian_variance $guass_noise & 
