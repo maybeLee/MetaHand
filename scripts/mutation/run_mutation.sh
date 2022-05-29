d66eddb0d86a789ac50ad348ae5f328295672d9e
Your Name <you@example.com>
2022-05-29 17:21:35 +0800

    feature: merge random erase and guassian

--- scripts/mutation/run_mutation.sh
+++ scripts/mutation/run_mutation.sh
@@ -10,7 +10,7 @@ mkdir -p /data/litszon/itf/ITF/2165-143-89-238-111.ap.ngrok.io/mutate
 for rand_erase in 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0
 do
 for guass_noise in 0.0 0.1 0.5 1.0 2.0 4.0 8.0 16.0
-done
+do
     echo "fixMutRatio_fixXY\n"
     python -O mutation_operation.py --image_path $img --label_path $label --mutate_path $mutate --random_erase $rand_erase --random_erase_mode fix_mut_ratio_fixxy --guassian_variance $guass_noise & 
     echo "varyMutRatio_fixXY\n"
