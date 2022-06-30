a7f7bcb569ba74fcef93b8e7f2b4ba4806839e5b
Your Name <you@example.com>
2022-06-30 21:51:33 +0800

    running

--- scripts/mutation/run_mutation.sh
+++ scripts/mutation/run_mutation.sh
@@ -8,7 +8,7 @@
 img=/ssddata/metahand/coco/images/
 label=/ssddata/metahand/coco/labels/
 mutate=/ssddata/metahand/coco/mutate/
-o_or_b=object
+o_or_b=object #object or background
 which_dataset="company"
 
 if [[ $which_dataset == "company" ]]
@@ -20,9 +20,10 @@ then
 img=/ssddata/metahand/data_egohands/images/train/
 label=/ssddata/metahand/data_egohands/images/train/
 mutate=/ssddata/metahand/data_egohands/mutate/
-chmod 777 -R $mutate
 fi
 
+mkdir -p $mutate
+chmod 777 -R $mutate
 # mkdir -p ${mutate}
 # mkdir -p ${mutate}log #for recording which process finishes during multi-processing
 # rand_erase=0.0
@@ -30,7 +31,7 @@ fi
 # for guass_noise in 0.0 0.5 1.0 2.0 4.0 8.0 16.0 32.0 64.0 128.0
 for guass_noise in 0.0
 do
-for rand_erase in 0.1 0.2 0.3 0.4 0.6 0.7 0.8 0.9 1.0
+for rand_erase in 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0
 do
     # echo "fixMutRatio_fixXY\n"
     python -O mutation_operation.py --image_path $img --label_path $label --mutate_path $mutate --random_erase $rand_erase --random_erase_mode fixMutRatio_centerXY --guassian_sigma $guass_noise --dataset $which_dataset --object_or_background $o_or_b 
