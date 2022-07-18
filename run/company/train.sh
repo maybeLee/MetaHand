bd8738a8e6a66d05ae83fc6c726fdafac53df3bd
LU Yao <dlproj@sccpu3.cse.ust.hk>
2022-07-18 21:54:55 +0800

    finish company scripts, achieve 5% improvement

--- run/company/train.sh
+++ run/company/train.sh
@@ -1,10 +1,11 @@
-cd ../
-TRAIN_ID=./data/training_id.txt
-IMGDIR=./data/ImageSet
-LABELDIR=./data/Labels
-WORKDIR=./data/working_dir/origin_model
-GPU=0
+cd ../../
+TRAIN_ID=./data_company/training_id.txt
+IMGDIR=./data_company/ImageSet
+LABELDIR=./data_company/Labels
+WORKDIR=./data_company/working_dir/origin_model
+GPU=0,1,2
 CFGPATH=./cfg/cross-hands.cfg
 OBJPATH=${WORKDIR}/obj.data
 python -u -m scripts.train.prepare_train_data --source_path=${TRAIN_ID} --img_dir=${IMGDIR} --label_dir=${LABELDIR} --target_dir=${WORKDIR}
 python -u -m scripts.train.train --obj_path=${OBJPATH} --cfg_path=$CFGPATH --retrain=1 --gpu=$GPU
+echo "Finish Training"
