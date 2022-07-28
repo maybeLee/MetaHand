2ef8042dfe63d88f70200bf0bb3ddf4566a8aa9f
mayebLee <maybelee98@gmail.com>
2022-07-28 16:54:51 +0800

    remove unnecessary scripts

--- run/aug_b_2.sh
+++
@@ -1,8 +0,0 @@
-cd ../
-MutateName=RandomNoiseB
-python -u -m scripts.evaluation.evaluate -mi=./data/RandomNoiseMutation/$MutateName --threshold=0.05
-mkdir -p ./data/working_dir/RandomNoiseMutation/${MutateName}_th08/
-mv ${MutateName}_violations.txt ./data/working_dir/RandomNoiseMutation/${MutateName}_th08/${MutateName}_violations.txt
-python -u -m scripts.train.prepare_train_data --source_path=./data/working_dir/RandomNoiseMutation/${MutateName}_th08/${MutateName}_violations.txt --img_dir=./data/RandomNoiseMutation/$MutateName --label_dir=./data/RandomNoiseMutation/label --target_dir=./data/working_dir/RandomNoiseMutation/${MutateName}_th08/data/
-# python -u -m scripts.train.train --obj_path=./data/working_dir/aug_obj/obj.data --cfg_path=./cfg/cross-hands.cfg --retrain=0 --pretrained_path=./data/working_dir/origin_model/backup/cross-hands_best.weights
-python -u -m scripts.train.train --obj_path=./data/working_dir/RandomNoiseMutation/${MutateName}_th08/data/obj.data --cfg_path=./cfg/cross-hands.cfg --retrain=1
--- run/train_coco.sh
+++
@@ -1,9 +0,0 @@
-cd ../
-TRAIN_ID=./data_coco/training_id.txt
-WORKDIR=./data_coco/working_dir/origin_model
-GPU=0,1,2
-CFGPATH=./cfg/yolov3.cfg
-OBJPATH=${WORKDIR}/obj.data
-DATASET=coco
-python -u -m scripts.train.prepare_train_data --source_path=${TRAIN_ID} --img_dir=${IMGDIR} --label_dir=${LABELDIR} --target_dir=${WORKDIR} --dataset=${DATASET}
-python -u -m scripts.train.train --obj_path=${OBJPATH} --cfg_path=$CFGPATH --retrain=1 --gpu=$GPU
