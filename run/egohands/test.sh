447c6e8b073cb1657e7b45672ca1c328ee23c3b9
mayebLee <maybelee98@gmail.com>
2022-08-07 16:46:08 +0800

    update file mode

--- run/egohands/test.sh
+++ run/egohands/test.sh
@@ -1,6 +1,10 @@
 # This test script is used to evaluate the trained model and augmented model on the test set
-obj_path="./data_egohands/working_dir/BackgroundGaussianMutation/BackgroundGaussian16_0_0.3/data/obj.data"
-cfg_path="./cfg/egohands.cfg"
-weights_path="./data_egohands/working_dir/BackgroundGaussianMutation/BackgroundGaussian16_0_0.3/data/backup/egohands_final.weights"
-gpu="0,1,2"
-python -u -m scripts.test.test --obj_path=$obj_path --cfg_path=$cfg_path --weights_path=$weights_path --gpu=$gpu
+std_list="0_5 1_0 2_0 4_0 8_0 16_0 32_0 64_0 128_0"
+for std in $std_list
+do
+  obj_path="./data_egohands/working_dir/BackgroundGaussianMutation/BackgroundGaussian${std}_0.3/data/obj.data"
+  cfg_path="./cfg/egohands.cfg"
+  weights_path="./data_egohands/working_dir/BackgroundGaussianMutation/BackgroundGaussian${std}_0.3/data/backup/egohands_final.weights"
+  gpu="0,1,2"
+  python -u -m scripts.test.test --obj_path=$obj_path --cfg_path=$cfg_path --weights_path=$weights_path --gpu=$gpu
+done
