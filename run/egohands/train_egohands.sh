af991d1af2a20f9984e53d6113146c4b6736ee8f
mayebLee <maybelee98@gmail.com>
2022-09-28 14:22:08 +0800

    update

--- run/egohands/train_egohands.sh
+++ run/egohands/train_egohands.sh
@@ -9,5 +9,6 @@ OBJPATH=${WORKDIR}/obj.data
 DATASET=egohands
 LOGDIR=logs/egohands/testing
 LOGFILE=${LOGDIR}/train_origin.log
+rm -rf ${WORKDIR}
 python -u -m scripts.train.prepare_train_data --source_path=${TRAIN_ID} --img_dir=${IMGDIR} --label_dir=${LABELDIR} --target_dir=${WORKDIR} --dataset=${DATASET} > ${LOGFILE}
 python -u -m scripts.train.train --obj_path=${OBJPATH} --cfg_path=$CFGPATH --retrain=1 --gpu=$GPU >> ${LOGFILE}
--- run/mutate.sh
+++ run/mutate.sh
@@ -8,8 +8,8 @@ then
     mutate=/root/data_company/ObjactGaussianMutation/ #Company dataset变异图像的路径
 elif [[ ${which_dataset} == "ego" ]]
 then
-    img=/root/data_egohands/ImageSet/   #Public dataset原始图像的路径
-    label=/root/data_egohands/Labels/ #Public dataset标签的路径
+    img=/root/data_egohands/images/train/   #Public dataset原始图像的路径
+    label=/root/data_egohands/labels/ #Public dataset标签的路径
     mutate=/root/data_egohands/ObjactGaussianMutation/      #Public dataset变异图像的路径
 else
     echo "Invalid programme input, expected 'company' or 'ego' but got ${which_dataset}"
