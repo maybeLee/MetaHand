6c3a46d7738f3607b3a528eae4cfe87d59785a24
mayebLee <maybelee98@gmail.com>
2022-12-24 17:08:11 +0800

    add coco training scripts

--- run/coco/train_coco.sh
+++ run/coco/train_coco.sh
@@ -3,11 +3,12 @@ TRAIN_ID=./data_coco/training_id.txt
 IMGDIR=./data_coco/coco/images
 LABELDIR=./data_coco/coco/labels
 WORKDIR=./data_coco/working_dir/testing/origin_model
-GPU=2
+GPU=0,1,2
 CFGPATH=./cfg/yolov3.cfg
 OBJPATH=${WORKDIR}/obj.data
 DATASET=coco
 LOGDIR=logs/coco/testing
+mkdir -p $LOGDIR
 LOGFILE=${LOGDIR}/train_origin.log
 rm -rf ${WORKDIR}
 python -u -m scripts.train.prepare_train_data --source_path=${TRAIN_ID} --img_dir=${IMGDIR} --label_dir=${LABELDIR} --target_dir=${WORKDIR} --dataset=${DATASET} > ${LOGFILE}
