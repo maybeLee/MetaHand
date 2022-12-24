0c0c371b00a372969946f54c9e5d4ddc6a41a780
mayebLee <maybelee98@gmail.com>
2022-12-24 16:02:51 +0800

    update coco training script

--- run/coco/train_coco.sh
+++ run/coco/train_coco.sh
@@ -1,13 +1,13 @@
 # cd ../../
-TRAIN_ID=./data_egohands/training_id.txt
-IMGDIR=./data_egohands/images
-LABELDIR=./data_egohands/labels
-WORKDIR=./data_egohands/working_dir/testing/origin_model
+TRAIN_ID=./data_coco/training_id.txt
+IMGDIR=./data_coco/coco/images
+LABELDIR=./data_coco/coco/labels
+WORKDIR=./data_coco/working_dir/testing/origin_model
 GPU=2
-CFGPATH=./cfg/egohands.cfg
+CFGPATH=./cfg/yolov3.cfg
 OBJPATH=${WORKDIR}/obj.data
-DATASET=egohands
-LOGDIR=logs/egohands/testing
+DATASET=coco
+LOGDIR=logs/coco/testing
 LOGFILE=${LOGDIR}/train_origin.log
 rm -rf ${WORKDIR}
 python -u -m scripts.train.prepare_train_data --source_path=${TRAIN_ID} --img_dir=${IMGDIR} --label_dir=${LABELDIR} --target_dir=${WORKDIR} --dataset=${DATASET} > ${LOGFILE}
