1b8f066267e4e0641b5b0aaf50e46d800f9d861b
maybeLee <maybelee98@gmail.com>
2023-01-04 14:18:27 +0800

    change mode

--- run/coco/continue_training.sh
+++ run/coco/continue_training.sh
@@ -11,5 +11,5 @@ OBJPATH=${WORKDIR}/obj.data
 CFGPATH=${WORKDIR}/yolov3.cfg
 GPU=0,1,2
 log_dir=logs/coco/testing/${MutateType}
-LOGFILE={log_dir}/${MutateName}_${th}.log
+LOGFILE=${log_dir}/${MutateName}_${th}.log
 python -u -m scripts.train.train --obj_path=${OBJPATH} --cfg_path=$CFGPATH --retrain=0 --pretrained_path=${PreTrainedPath} --gpu=$GPU >> ${LOGFILE}
