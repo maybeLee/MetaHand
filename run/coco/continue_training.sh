082a368255360ccb473479b302930d0fd897e940
maybeLee <maybelee98@gmail.com>
2023-01-04 14:15:30 +0800

    fix bug on the continue training scripts

--- run/coco/continue_training.sh
+++ run/coco/continue_training.sh
@@ -1,4 +1,5 @@
 # This is a utility script to continue training some models for analysis
+MutateType=ObjectGaussianMutation
 th=0.3
 ratio=0.2
 data_dir=data_coco
@@ -9,7 +10,6 @@ PreTrainedPath=${WORKDIR}/backup/yolov3_best.weights
 OBJPATH=${WORKDIR}/obj.data
 CFGPATH=${WORKDIR}/yolov3.cfg
 GPU=0,1,2
+log_dir=logs/coco/testing/${MutateType}
 LOGFILE={log_dir}/${MutateName}_${th}.log
-mkdir -p $LOGDIR
-LOGFILE=${LOGDIR}/train_origin.log
 python -u -m scripts.train.train --obj_path=${OBJPATH} --cfg_path=$CFGPATH --retrain=0 --pretrained_path=${PreTrainedPath} --gpu=$GPU >> ${LOGFILE}
