7776e2443ac83f4430c73f3f9d95d613367117aa
maybeLee <maybelee98@gmail.com>
2023-05-16 20:01:17 +0800

    Add configuration and cfg for yolov7

---
+++ run/yolov7_coco/train_coco.sh
@@ -0,0 +1,15 @@
+cd ../../
+TRAIN_ID=./data_coco/training_id.txt
+IMGDIR=./data_coco/coco/images
+LABELDIR=./data_coco/coco/labels
+WORKDIR=./data_coco/working_dir/yolov7/origin_model
+GPU=0,1,2
+CFGPATH=./cfg/yolov7.cfg
+OBJPATH=${WORKDIR}/obj.data
+DATASET=coco
+LOGDIR=logs/coco/yolov7
+mkdir -p $LOGDIR
+LOGFILE=${LOGDIR}/train_origin.log
+rm -rf ${WORKDIR}
+python -u -m scripts.train.prepare_train_data --source_path=${TRAIN_ID} --img_dir=${IMGDIR} --label_dir=${LABELDIR} --target_dir=${WORKDIR} --dataset=${DATASET} > ${LOGFILE}
+python -u -m scripts.train.train --obj_path=${OBJPATH} --cfg_path=$CFGPATH --retrain=1 --gpu=$GPU >> ${LOGFILE}
