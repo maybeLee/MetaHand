aad81bd8406d482c70c650c597a4ac47b6673b74
maybeLee <maybelee98@gmail.com>
2023-07-20 01:03:28 +0800

    For yolov7 single image detection

--- scripts/evaluation/detect_parallel_yolov7.py
+++ scripts/evaluation/detect_parallel_yolov7.py
@@ -74,6 +74,9 @@ class Detector(object):
         e_time = time.time()
         logger.info(f"Total Time Used: {e_time - s_time}")
 
+    def detect_single(self):
+        args = (self.weights_path, self.yolo_size, self.yolo_confidence, self.img_dir, None, None, self.save_dir, self.base_name)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
