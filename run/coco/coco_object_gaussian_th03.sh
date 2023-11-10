75bdc4d7db6fd3c7088cb1e6921bd3d279b56b9c
maybeLee <maybelee98@gmail.com>
2023-11-10 10:24:30 +0800

    Update two scripts evaluating YOLOv3 and YOLOv7 on COCO

--- run/coco/coco_object_gaussian_th03.sh
+++ run/coco/coco_object_gaussian_th03.sh
@@ -10,7 +10,8 @@ mkdir -p $log_dir
 mkdir -p $output_dir
 th=0.3
 # std_list="1_0 2_0 4_0 8_0 16_0 32_0 64_0 128_0"
-ratio_list="02"
+ratio_list="01 02 03 04 05 06 07 08 09"
+# ratio_list="01"
 # for std in $std_list
 num_epoch=100
 original_cfg_path=./cfg/yolov3.cfg
@@ -27,39 +28,40 @@ do
     -ol=./${data_dir}/labels \
     -w=${weights_path} \
     -od=${output_dir} \
+    -olf='not_segment' \
     --dataset=${DATASET} \
     --mr=${MR} \
-    --jobs=4 \
+    --jobs=8 \
     --threshold=${th} > ${log_dir}/${MutateName}_${th}.log
 
     base_dir=./${data_dir}/working_dir/testing/${MutateType}/${MutateName}_${th}
     mkdir -p $base_dir
     mv ${MutateName}_violations.txt ${base_dir}/${MutateName}_violations.txt
 
-    TRAIN_ID=${base_dir}/${MutateName}_violations.txt
-    IMGDIR=./${data_dir}/${MutateType}/$MutateName
-    WORKDIR=$base_dir/data
+    # TRAIN_ID=${base_dir}/${MutateName}_violations.txt
+    # IMGDIR=./${data_dir}/${MutateType}/$MutateName
+    # WORKDIR=$base_dir/data
 
-    python -u -m scripts.train.prepare_train_data \
-    --source_path=${TRAIN_ID} \
-    --img_dir=${IMGDIR} \
-    --label_dir=${LABELDIR} \
-    --target_dir=${WORKDIR} \
-    --dataset=${DATASET} --num_epoch=$num_epoch --cfg_path=$original_cfg_path >> ${log_dir}/${MutateName}_${th}.log
+    # python -u -m scripts.train.prepare_train_data \
+    # --source_path=${TRAIN_ID} \
+    # --img_dir=${IMGDIR} \
+    # --label_dir=${LABELDIR} \
+    # --target_dir=${WORKDIR} \
+    # --dataset=${DATASET} --num_epoch=$num_epoch --cfg_path=$original_cfg_path >> ${log_dir}/${MutateName}_${th}.log
 
 done
 
-gpu_id=0,1,2
-for ratio in $ratio_list
-do
-    MutateName=object_gaussian_160_fixMutRatio_centerXY_${ratio}
-    base_dir=./${data_dir}/working_dir/testing/${MutateType}/${MutateName}_${th}
-    WORKDIR=$base_dir/data
-    CFGPATH=${WORKDIR}/yolov3.cfg
-    OBJPATH=${WORKDIR}/obj.data
-    base_dir=./${data_dir}/working_dir/testing/${MutateType}/${MutateName}_${th}
-    python -u -m scripts.train.train --obj_path=${OBJPATH} --cfg_path=$CFGPATH --retrain=1 --gpu=$gpu_id >> ${log_dir}/${MutateName}_${th}.log
-done
+# gpu_id=0,1,2
+# for ratio in $ratio_list
+# do
+#     MutateName=object_gaussian_160_fixMutRatio_centerXY_${ratio}
+#     base_dir=./${data_dir}/working_dir/testing/${MutateType}/${MutateName}_${th}
+#     WORKDIR=$base_dir/data
+#     CFGPATH=${WORKDIR}/yolov3.cfg
+#     OBJPATH=${WORKDIR}/obj.data
+#     base_dir=./${data_dir}/working_dir/testing/${MutateType}/${MutateName}_${th}
+#     python -u -m scripts.train.train --obj_path=${OBJPATH} --cfg_path=$CFGPATH --retrain=1 --gpu=$gpu_id >> ${log_dir}/${MutateName}_${th}.log
+# done
 
 echo $"Finish All Jobs"
 
--- run/yolov7_coco/yolov7_object_gaussian_th03.sh
+++ run/yolov7_coco/yolov7_object_gaussian_th03.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-cd ../../
+# cd ../../
 MutateType=ObjectGaussianMutation
 DATASET=yolov7
 GPU=0,1,2
 data_dir=tools/yolov7/coco
 log_dir=logs/yolov7/${MutateType}
-weights_path=./tools/yolov7/runs/train/yolov7/weights/best.pt
+weights_path=./tools/yolov7/runs/train/yolov7/weights/epoch_199.pt
 output_dir=./tools/yolov7/runs/detect
 mkdir -p $log_dir
 mkdir -p $output_dir
 th=0.3
 # std_list="1_0 2_0 4_0 8_0 16_0 32_0 64_0 128_0"
-ratio_list="03"
+ratio_list="01 02 03 04 05 06 07 08 09"
 # for std in $std_list
 for ratio in $ratio_list
 do
@@ -19,16 +19,18 @@ do
     MR=2
     LABELDIR=same
     echo "Preparing Data For ${MutateName}"
-    IMGDIR=./${data_dir}/${MutateType}/${MutateType}/${MutateName}
+    IMGDIR=./${data_dir}/${MutateType}/${MutateName}
     python -u -m scripts.evaluation.evaluate \
-    -oi=./${data_dir}/images/train2017 \
+    -oi=./${data_dir}/images \
     -mi=${IMGDIR} \
-    -ol=./${data_dir}/labels/train2017 \
+    -ol=./${data_dir}/labels \
+    -olf='yolov7' \
     -w=${weights_path} \
     -od=${output_dir} \
     --dataset=${DATASET} \
     --mr=${MR} \
-    --jobs=8 \
+    -img_size=320 \
+    --jobs=12 \
     --threshold=${th} > ${log_dir}/${MutateName}_${th}.log
 
     base_dir=./tools/yolov7/runs/train/${MutateType}/${MutateName}_${th}
@@ -36,30 +38,30 @@ do
     mkdir -p $base_dir
     mv ${MutateName}_violations.txt ${base_dir}/${MutateName}_violations.txt
 
-    # new train file will be saved in ./{base_dir}/train.txt
-    python -u -m scripts.train.prepare_train_data \
-    --source_path=${base_dir}/${MutateName}_violations.txt \
-    --target_dir=${base_dir} \
-    --dataset=${DATASET}
+    # # new train file will be saved in ./{base_dir}/train.txt
+    # python -u -m scripts.train.prepare_train_data \
+    # --source_path=${base_dir}/${MutateName}_violations.txt \
+    # --target_dir=${base_dir} \
+    # --dataset=${DATASET}
 
-    train_txt=${v7_base}/train.txt
-    cp ./tools/yolov7/data/coco.yaml ${base_dir}/coco.yaml
-    # This line should use \" instead of \'
-    sed -i "s|train: .\/coco\/train2017.txt  # 118287 images|train: ${train_txt}|" ${base_dir}/coco.yaml
+    # train_txt=${v7_base}/train.txt
+    # cp ./tools/yolov7/data/coco.yaml ${base_dir}/coco.yaml
+    # # This line should use \" instead of \'
+    # sed -i "s|train: .\/coco\/train2017.txt  # 118287 images|train: ${train_txt}|" ${base_dir}/coco.yaml
 
-    gpu_id=0,1,2
-    cd tools/yolov7
-    python -m torch.distributed.launch --nproc_per_node 3 \
-    --master_port 9527 train.py \
-    --workers 8 \
-    --device ${gpu_id} --sync-bn \
-    --batch-size 66 \
-    --data ${v7_base}/coco.yaml \
-    --img 320 320 --cfg cfg/training/yolov7.yaml \
-    --weights '' \
-    --name yolov7_${MutateName} \
-    --hyp data/hyp.scratch.p5.yaml
-    cd ../../
+    # gpu_id=0,1,2
+    # cd tools/yolov7
+    # python -m torch.distributed.launch --nproc_per_node 3 \
+    # --master_port 9527 train.py \
+    # --workers 8 \
+    # --device ${gpu_id} --sync-bn \
+    # --batch-size 66 \
+    # --data ${v7_base}/coco.yaml \
+    # --img 320 320 --cfg cfg/training/yolov7.yaml \
+    # --weights '' \
+    # --name yolov7_${MutateName} \
+    # --hyp data/hyp.scratch.p5.yaml
+    # cd ../../
 done
 
 echo $"Finish All Jobs"
